b60f49cb70166baf6177a8cc23d7644b43b15072
Wolfgang Schreurs <wolfgang@wolftrail.net>
2020-05-26 11:38:01 +0700

    Code clean-up

--- DungeonBuilder/Room.swift
+++ DungeonBuilder/Room.swift
@@ -12,18 +12,17 @@ public class Room {
     let i: Int
     let j: Int
 
-    public var coord: Coordinate { return Coordinate(i, j) }    
-    
+    lazy var north: Int = { return self.i * 2 + 1 }()
+    lazy var south: Int = { return (self.i + self.height) * 2 + 1 }()
+    lazy var east: Int = { return (self.j + self.width) * 2 + 1 }()
+    lazy var west: Int = { return self.j * 2 + 1 }()
+
+    var doors: [Direction: [Door]] = [:]
+
+    public var coord: Coordinate { return Coordinate(i, j) }
     public let width: Int
     public let height: Int
-            
-    public lazy var north: Int = { return self.i * 2 + 1 }()
-    public lazy var south: Int = { return (self.i + self.height) * 2 + 1 }()
-    public lazy var east: Int = { return (self.j + self.width) * 2 + 1 }()
-    public lazy var west: Int = { return self.j * 2 + 1 }()
     public lazy var area: Int = { return self.width * self.height }()
-    
-    var doors: [Direction: [Door]] = [:]
 
     init(i: Int, j: Int, width: Int, height: Int) {
         self.i = i
